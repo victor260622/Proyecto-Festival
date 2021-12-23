a9b5cee450394c3fe4dbc3b6e21449552a8b41bc
Victor Lovera <victor260622@gmail.com>
2021-12-22 23:26:23 -0300

    Inicio del proyecto musical

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+const { src, dest, watch, parallel } = require("gulp");
+const sass= require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
+const autoprefixer = require ("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
+
+const cache = require("gulp-cache")
+const imagemin = require("gulp-imagemin");
+const  webp= require("gulp-webp");
+
+
+const terser = require("gulp-terser-js");
+
+function css(done){
+    src("src/scss/**/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+        .pipe( sass() )
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write("."))
+        .pipe( dest("build/css") )
+
+    done();
+};
+
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src("src/img/**/*.{png,jpg}")
+    .pipe(cache(imagemin(opciones)))
+    .pipe(dest("build/img"))
+    
+    done();
+
+}
+
+function versionWebp( done ){
+
+    const opciones = {
+        qualit: 50
+    };
+    src("src/img/**/*.{png,jpg}")
+    .pipe(webp(opciones))
+    .pipe(dest("build/img"));
+
+    done();
+
+};
+function javascript(done) {
+    src("src/js/**/*.js")
+    .pipe(terser())
+    .pipe(dest("build/js"));
+    done()
+}
+
+function dev ( done ) {
+    watch("src/scss/**/*.scss", css);
+    watch("src/js/**/*.js", javascript);
+
+    done();
+};
+
+exports.css = css;
+exports.js = javascript;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(imagenes, versionWebp, javascript, dev);
